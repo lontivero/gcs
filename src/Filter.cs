27854b8fcf71995100b8b733df43e44e89a9b603
lontivero <lucasontivero@gmail.com>
2018-02-08 12:17:57 -0300

    Using index to improve the searching performance over huge data sets

--- src/Filter.cs
+++ src/Filter.cs
@@ -6,6 +6,18 @@ using System.Threading.Tasks;
 
 namespace GolombCodeFilterSet
 {
+	public class IndexedFilter
+	{
+		public BitArray Data { get; }
+		public List<(ulong, int)> Index { get; }
+
+		public IndexedFilter(BitArray data, List<(ulong, int)> index)
+		{
+			Data = data;
+			Index = index;
+		}
+	}
+
 	// Implements a Golomb-coded set to be use in the creation of client-side filter
 	// for a new kind Bitcoin light clients. This code is based on the BIP:
 	// https://github.com/Roasbeef/bips/blob/master/gcs_light_client.mediawiki
@@ -15,30 +27,32 @@ namespace GolombCodeFilterSet
 		public int N { get; internal set; }
 		public ulong ModulusP { get; internal set; }
 		public ulong ModulusNP { get; internal set; }
-		public byte[] Data { get; internal set; }
-
+		public IndexedFilter IndexedFilter { get; internal set; }
+		private BitArray _bitArray;
+		
 		public static Filter Build(byte[] k, byte P, IEnumerable<byte[]> data)
 		{
 			// NOTE: P must be a power of two as we target the specialized case of Golomb coding: Golomb - Rice coding
 			if (P == 0x00 || (P & (P - 1)) != 0)
 				throw new ArgumentException("P has to be a power of two value", nameof(P));
 
-			if (data == null || !data.Any())
+			var bytesData = data as byte[][] ?? data.ToArray();
+			if (data == null || !bytesData.Any())
 				throw new ArgumentException("data can not be null or empty array", nameof(data));
 
 			var modP = 1UL << P;
 
-			var hs = ConstructHashedSet(P, k, data);
+			var hs = ConstructHashedSet(P, k, bytesData);
 			var filterData = Compress(hs, P);
-			var N = data.Count();
+			var N = bytesData.Length;
 
 			return new Filter
 			{
 				P = P,
-				N = data.Count(),
+				N = N,
 				ModulusP = modP,
 				ModulusNP = ((ulong)N) * modP,
-				Data = filterData
+				IndexedFilter = filterData
 			};
 		}
 
@@ -68,29 +82,57 @@ namespace GolombCodeFilterSet
 			return ret;
 		}
 
-		private static byte[] Compress(List<ulong> values, byte P)
+		private static IndexedFilter Compress(List<ulong> values, byte P)
 		{
-			var bitStream = new BitStream();
+			var indexCount = (values.Count / 128) + 1;
+			var filterIndex = new List<(ulong, int)>(indexCount);
+			var bitArray = new BitArray();
+			var bitStream = new BitStream(bitArray);
 			var sw = new GRCodedStreamWriter(bitStream, P);
 
+			var i = 0;
+			var pos = 0;
+			var lastValue = 0UL;
 			foreach (var value in values)
 			{
-				sw.Write(value);
+				if(i % indexCount == 0) 
+					filterIndex.Add((lastValue, pos));
+				pos = sw.Write(value);
+				lastValue = value;
+				i++;
 			}
-			return bitStream.ToByteArray();
+			return new IndexedFilter(bitArray, filterIndex);
 		}
 
 
 		public bool Match(byte[] data, byte[] key)
 		{
-			var bitStream = new BitStream(Data);
-			var sr = new GRCodedStreamReader(bitStream, P);
-
 			var nphi = ModulusNP >> 32;
 			var nplo = ((ulong)((uint)ModulusNP));
 
 			var hash = SipHasher.Hash(key, data);
 			var searchValue = FastReduction(hash, nphi, nplo);
+
+			var lastPos = 0;
+			var pos = 0;
+			var lastValue = 0UL;
+			var value = 0UL;
+			foreach (var index in IndexedFilter.Index)
+			{
+				lastPos = pos;
+				lastValue = value;
+				if (index.Item1 > searchValue)
+					break;
+
+				pos = index.Item2;
+				value = index.Item1;
+			}
+
+			var bitStream = new BitStream(IndexedFilter.Data);
+			bitStream.Position = lastPos;
+			var sr = new GRCodedStreamReader(bitStream, P, lastValue);
+			if (lastValue == searchValue)
+				return true;
 			try
 			{
 				var currentValue = sr.Read();
@@ -120,8 +162,8 @@ namespace GolombCodeFilterSet
 			var lastValue2 = hs[0];
 			var i = 1;
 
-			var bitStream = new BitStream(Data);
-			var sr = new GRCodedStreamReader(bitStream, P);
+			var bitStream = new BitStream(IndexedFilter.Data);
+			var sr = new GRCodedStreamReader(bitStream, P, 0);
 
 			while (lastValue1 != lastValue2)
 			{
---
+++ tests/BitArrayTest.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using GolombCodeFilterSet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GolombCodedFilterSet.UnitTests
+{
+	[TestClass]
+	public class FastBitArrayTest
+	{
+		[TestMethod]
+		public void GetBitsTest()
+		{
+			// 1 1 1 0 1 0 1 1 - 1 0 1 0 1 0 1 1 - 1 0 1 0 1 1 1 0 - 1 0 1 0 1 1 1 0 
+			// 1 0 1 1 1 0 1 0 
+			var barr = new BitArray();
+			barr.Length = 50;
+			for (int i = 0; i < 40; i++)
+			{
+				if (i % 7 == 0)
+				{
+					barr[i] = true;
+					i++;
+					barr[i] = true;
+				}
+				else
+				{
+					barr[i] = i % 2 == 0;
+				}
+			}
+
+			// Get bits in the same int
+			Assert.AreEqual((ulong) 0b111, barr.GetBits(0, 3));
+			Assert.AreEqual((ulong) 0b10111, barr.GetBits(0, 5));
+			Assert.AreEqual((ulong) 0b01010111010, barr.GetBits(3, 11));
+
+			// Get bits in cross int
+			Assert.AreEqual((ulong) 0b101110101110101, barr.GetBits(24, 16));
+		}
+
+		[TestMethod]
+		public void SetRandomBitsTest()
+		{
+			var barr = new BitArray(new byte[0]);
+			barr.Length = 150;
+			var values = new List<int>();
+			var lengths = new List<int>();
+			var rnd = new Random();
+			var pos = 0;
+
+			for (int i = 0; i < 10; i++)
+			{
+				var val = rnd.Next();
+				var len = rnd.Next(1, 20);
+				barr.SetBits(pos, (ulong) val, len);
+
+				values.Add(val);
+				lengths.Add(len);
+				pos += len;
+			}
+
+			pos = 0;
+			for (int i = 0; i < 10; i++)
+			{
+				var len = lengths[i];
+				var expectedValue = values[i];
+				var value = barr.GetBits(pos, len);
+				Assert.AreEqual(((ulong) expectedValue & value), value);
+				pos += len;
+			}
+		}
+
+		[TestMethod]
+		public void SetBitAndGetBitsTest()
+		{
+			var barr = new BitArray(new byte[0]);
+			barr.Length = 150;
+
+			var j = true;
+			for (int i = 0; i < 64; i += 2)
+			{
+				if (j)
+				{
+					barr.SetBit(i, true);
+					barr.SetBit(i + 1, true);
+				}
+				else
+				{
+					barr.SetBit(i, false);
+					barr.SetBit(i + 1, false);
+				}
+
+				j = !j;
+			}
+
+			for (int i = 0; i < 16; i++)
+			{
+				Assert.AreEqual(0b11UL, barr.GetBits(i * 4, 2));
+				Assert.AreEqual(0b00UL, barr.GetBits((i * 4) + 2, 2));
+			}
+
+			for (int i = 0; i < 8; i++)
+			{
+				Assert.AreEqual(0b0011UL, barr.GetBits(i * 8, 4));
+				Assert.AreEqual(0b0011UL, barr.GetBits((i * 8) + 4, 2));
+			}
+
+			Assert.AreEqual(0b11001UL, barr.GetBits(29, 5));
+		}
+
+		public void SetBitsBigEndianTest()
+		{
+			var barr = new BitArray(new byte[0]);
+			barr.Length = 5;
+			barr.SetBits(0, 14, 4);
+			var val = barr.GetBits(0, 4);
+
+			barr = new BitArray(new byte[0]);
+			barr.Length = 5;
+			barr.SetBit(0, false);
+			barr.SetBit(1, true);
+			barr.SetBit(2, true);
+			barr.SetBit(3, true);
+			val = barr.GetBits(0, 4);
+		}
+	}
+}
